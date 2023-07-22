ec3a9dc25af0f164171839afa5fb3f2a2be48bfe
Nissi <semanyohn@gmail.com>
2023-07-22 23:06:42 +0000

    Implement current month birthday celebrations analytic card

--- members/urls.py
+++ members/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('add/', views.add, name='add'),
     path('edit/<int:id>/', views.edit, name='edit'),
     path('delete/<int:id>/', views.delete, name='delete'),
+    path('logout/', views.logout_view, name='logout')
+
 ]
--- members/views.py
+++ members/views.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 
 from .models import Member
 from .forms import MemberForm
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from datetime import date, timedelta
 
 
 # Create your views here.
@@ -14,20 +17,22 @@ def index(request):
     males = Member.objects.filter(gender='M')
     females = Member.objects.filter(gender='F')
     married = Member.objects.filter(marital_status='R')
+    current_months_birthdays = get_members_with_birthdays_in_current_month()
     context = {
         'members': members,
         'males': males,
         'females': females,
-        'married': married
+        'married': married,
+        'current_months_birthdays': current_months_birthdays
     }
     return render(request, 'members/index.html', context)
 
-
+@login_required
 def view_member(request, id):
     member = Member.objects.get(pk=id)
     return HttpResponseRedirect(reverse('index'))
 
-
+@login_required
 def add(request):
     if request.method == 'POST':
         form = MemberForm(request.POST)
@@ -40,7 +45,7 @@ def add(request):
         'form': form
     })
 
-
+@login_required
 def edit(request, id):
     if request.method == 'POST':
         member = Member.objects.get(pk=id)
@@ -58,9 +63,32 @@ def edit(request, id):
         'form': form
     })
 
-
+@login_required
 def delete(request, id):
     if request.method == 'POST':
         member = Member.objects.get(pk=id)
         member.delete()
     return HttpResponseRedirect(reverse('index'))
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/accounts/login')
+
+def get_members_with_birthdays_in_current_month():
+    today = date.today()
+    # end_date = today + timedelta(days)
+
+    # Extract month and day from today's date
+    current_month = today.month
+    current_day = today.day
+
+    # Extract month and day from the end_date
+    # end_month = end_date.month
+    # end_day = end_date.day
+
+    filtered_Members = Member.objects.filter(
+            date_of_birth__month=current_month,
+            date_of_birth__day__gte=current_day,            
+        )
+    return filtered_Members
